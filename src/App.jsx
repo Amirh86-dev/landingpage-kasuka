31cf646013d706a17da157daa261096b6e481baf
Amirh86-dev <birdwolf913@gmail.com>
2025-07-29 19:05:42 +0330

    feat: add navbar to project and updating style directory

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>
+        متن تستی
+      </h1>
+    </div>
+  );
+}
+
+export default App;
