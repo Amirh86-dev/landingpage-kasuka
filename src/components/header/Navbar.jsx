aa5826a9e36473fa14ae65cf5b3e0d49c99e282c
Amirh86-dev <birdwolf913@gmail.com>
2025-07-29 20:55:17 +0330

    style(Navbar): Sorting codes

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -6,42 +6,42 @@ export default class Navbar extends Component {
     return (
       <div className={styled.parent}>
         <div className={`container ${styled.parentWrraper}`}>
-            <a href="" className={styled.btnSecStart}>
-              شروع
-            </a>
-            <nav>
-              <ul className={styled.menu}>
-                <li>
-                  <a href="">خانه</a>
-                </li>
-                <li>
-                  <a href="">درباره ی ما</a>
-                </li>
-                <li>
-                  <a href="">خدمات</a>
-                </li>
-                <li>
-                  <a href="">نمونه کارها</a>
-                </li>
-                <li>
-                  <a href="">تیم</a>
-                </li>
-                <li>
-                  <a href="">لیست کشویی</a>
-                </li>
-                <li>
-                  <a href="">تماس با ما</a>
-                </li>
-              </ul>
-            </nav>
-            <div className={styled.logoSec}>
-              <h1 className={styled.headerTitle}>Kasuka</h1>
-              <img
-                src="assets/images/logo.png"
-                alt=""
-                className={styled.logoImg}
-              />
-            </div>
+          <a href="" className={styled.btnSecStart}>
+            شروع
+          </a>
+          <nav>
+            <ul className={styled.menu}>
+              <li>
+                <a href="">خانه</a>
+              </li>
+              <li>
+                <a href="">درباره ی ما</a>
+              </li>
+              <li>
+                <a href="">خدمات</a>
+              </li>
+              <li>
+                <a href="">نمونه کارها</a>
+              </li>
+              <li>
+                <a href="">تیم</a>
+              </li>
+              <li>
+                <a href="">لیست کشویی</a>
+              </li>
+              <li>
+                <a href="">تماس با ما</a>
+              </li>
+            </ul>
+          </nav>
+          <div className={styled.logoSec}>
+            <h1 className={styled.headerTitle}>Kasuka</h1>
+            <img
+              src="assets/images/logo.png"
+              alt=""
+              className={styled.logoImg}
+            />
+          </div>
         </div>
       </div>
     );
