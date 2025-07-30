807dfd164dd7a63128516c5313acf4601ccf85ee
Amirh86-dev <birdwolf913@gmail.com>
2025-07-31 02:21:33 +0330

    feat: complete header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
-import Navbar from "./Navbar";
+import styled from "./header.module.css";
+import InfoBoxContainer from "./InfoBoxContainer";
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-      </div>
+      <header>
+        <section className={styled.secHeader}>
+          <div className={styled.grayFilter}>
+              <h1 className={styled.headTitr}>
+                <span>راه حل های قدرتمند دیجیتال با</span>
+                <br />
+                <span className="text-orange">Kasuka</span>
+              </h1>
+              <h3 className={styled.lessTitr}>
+                ما تیمی از بازاریابان با استعداد دیجیتال هستیم
+              </h3>
+              <div>
+                <InfoBoxContainer />
+              </div>
+          </div>
+        </section>
+      </header>
     );
   }
 }
