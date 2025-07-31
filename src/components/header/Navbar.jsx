6fa2798484e874b317dbaefae49bcdccf1367fe3
Amirh86-dev <birdwolf913@gmail.com>
2025-07-31 17:26:59 +0330

    feat(navbar): change css style after scroll

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -2,9 +2,31 @@ import React, { Component } from "react";
 import styled from "./header.module.css";
 
 export default class Navbar extends Component {
+  state = {
+    scroll: false,
+  };
+
+  scrollHandler = () => {
+    if (window.scrollY >= 50) {
+      this.setState({ scroll: true });
+    } else {
+      this.setState({ scroll: false });
+    }
+  };
+
   render() {
+    console.log(window.scrollY);
+
+    window.addEventListener("scroll", this.scrollHandler);
     return (
-      <div className={styled.parent}>
+      <div
+        className={styled.parent}
+        style={{
+          backgroundColor: this.state.scroll
+            ? "rgba(0, 0, 0, .75)"
+            : "transparent",
+        }}
+      >
         <div className={`container ${styled.parentWrraper}`}>
           <a href="" className={styled.btnSecStart}>
             شروع
